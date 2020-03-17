b57245bc412f530aa33bdf8266e469a9f92c8fe5
Syeikh Anugrah <syeikhanugrah@gmail.com>
2020-03-17 16:55:05 +0700

    set bracket spacing prettier rule to true

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
-import React, {Component} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
 
 class App extends Component {
   state = {
     count: 0,
   };
 
+  _increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
       <View style={styles.view}>
         <Text>{this.state.count}</Text>
+        <Button title="+" onPress={this._increment} />
       </View>
     );
   }
