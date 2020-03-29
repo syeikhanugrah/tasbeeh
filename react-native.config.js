8726618cb83edd126fbc6456a447a3b816cf80b4
Syeikh Anugrah <syeikhanugrah@gmail.com>
2020-03-29 20:17:57 +0700

    Tell react native asset link react-native-vector-icons for ios is not an error

--- react-native.config.js
+++ react-native.config.js
@@ -2,7 +2,8 @@ module.exports = {
   dependencies: {
     'react-native-vector-icons': {
       platforms: {
-        android: null, // disable Android platform, other platforms will still autolink
+        android: null,
+        ios: null,
       },
     },
   },
